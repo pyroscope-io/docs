145200985ebb96bd1927f78cabeacc498ac804d4
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-08-29 14:22:29 +0300

    chore: optimize some images

--- src/components/Homepage/shared/Logo/index.jsx
+++ src/components/Homepage/shared/Logo/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './styles.module.scss';
+import LogoIcon from '/img/logo-v3.svg'
 
 const Logo = () => (
     <div className={styles.headerLogoWrapper}>
         <a href='#'>
-            <img src='/img/logo-v3-small.png' />
+            <LogoIcon />
             <span className={styles.appName}>Pyroscope</span>
         </a>
     </div>
