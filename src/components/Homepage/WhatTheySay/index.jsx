5a5c83903ff7ac4d625ab587db468492fafc6f40
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-08-31 16:45:31 +0300

    feat: responsiveness for Comments section

--- src/components/Homepage/WhatTheySay/index.jsx
+++ src/components/Homepage/WhatTheySay/index.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import SliderArrow from '/img/homepage/slider_arrow.svg';
 import QuoteIcon from '/img/homepage/quote_icon.svg';
 import BgSphere from '/img/homepage/sphere.svg';
 import SectionWrapper from '../shared/SectionWrapper';
+import Carousel from 'nuka-carousel';
 
 const comment = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip."
 
-// fixes incorrect html atributes on prev/next buttons
-const SlickButtonFix = ({ currentSlide, slideCount, children, ...props }) => (
-    <span {...props}>{children}</span>
-);
+const useSlidesToShow = () => {
+    const [screenSize, setIsMobile] = useState();
+    useEffect(() => {
+        const onResize = () => {
+            setIsMobile(window?.innerWidth);
+        }
+
+        window.addEventListener("resize", onResize);
+
+        setIsMobile(window?.innerWidth);
+
+        return () => {
+            window.removeEventListener("resize", onResize);
+        }
+    }, []);
+
+    if (screenSize < 720) {
+        return 1
+    } else if (screenSize < 1050) {
+        return 2
+    } else {
+        return 3
+    }
+}
 
 const WhatTheySay = () => {
+    const slidesToShow = useSlidesToShow();
+
     return (
         <SectionWrapper className={styles.sectionWrapper}>
             <BgSphere className={styles.bgSphere} />
@@ -23,30 +43,80 @@ const WhatTheySay = () => {
             <div className={styles.sectionBackground} />
             <div className={styles.section}>
                 <h2 className={styles.title}>See What They Say</h2>
-
+                <div className={styles.carouselWrapper}>
+                    <Carousel
+                        renderBottomCenterControls={false}
+                        wrapAround={true}
+                        slidesToScroll={1}
+                        slidesToShow={slidesToShow}
+                        renderCenterLeftControls={({ previousSlide }) => (
+                            <SliderArrow onClick={previousSlide} />
+                        )}
+                        renderCenterRightControls={({ nextSlide }) => (
+                            <SliderArrow onClick={nextSlide} />
+                        )}
+                    >
+                        <Comment text={comment} userName="Maria Ladowska 1" />
+                        <Comment text={comment} userName="Steffany Jessica 2" />
+                        <Comment text={comment} userName="Chris Wei 3" />
+                        <Comment text={comment} userName="Maria Ladowska 4" />
+                        <Comment text={comment} userName="Steffany Jessica 5" />
+                        <Comment text={comment} userName="Chris Wei 6" />
+                        <Comment text={comment} userName="Maria Ladowska 7" />
+                        <Comment text={comment} userName="Steffany Jessica 8" />
+                        <Comment text={comment} userName="Chris Wei 9" />
+                    </Carousel>
+                </div>
             </div>
-            <div className={styles.carouselWrapper}>
-                <Slider
-                    centerMode={true}
-                    slidesToScroll={1}
-                    slidesToShow={3}
-                    infinite
-                    prevArrow={<SlickButtonFix><SliderArrow /></SlickButtonFix>}
-                    nextArrow={<SlickButtonFix><SliderArrow /></SlickButtonFix>}
-                    centerPadding="10%"
-                    speed={250}
+            {/* <div className={styles.carouselWrapper}>
+                <Slider {...settings}
+                    // centerMode={true}
+                    // slidesToScroll={3}
+                    // slidesToShow={3}
+                    // // infinite
+                    // prevArrow={<SlickButtonFix><SliderArrow /></SlickButtonFix>}
+                    // nextArrow={<SlickButtonFix><SliderArrow /></SlickButtonFix>}
+                    // centerPadding="10%"
+                    // speed={250}
+                // responsive={[{
+                //     breakpoint: 992,
+                //     settings: {
+                //         slidesToShow: 2,
+                //         slidesToScroll: 1,
+                //         infinite: true,
+                //     }
+                // }, {
+                //     breakpoint: 700,
+                //     settings: {
+                //         slidesToShow: 1,
+                //         slidesToScroll: 1
+                //     }
+                // }]}
                 >
-                    <Comment text={comment} userName="Maria Ladowska" />
-                    <Comment text={comment} userName="Steffany Jessica" />
-                    <Comment text={comment} userName="Chris Wei" />
-                    <Comment text={comment} userName="Maria Ladowska" />
-                    <Comment text={comment} userName="Steffany Jessica" />
-                    <Comment text={comment} userName="Chris Wei" />
-                    <Comment text={comment} userName="Maria Ladowska" />
-                    <Comment text={comment} userName="Steffany Jessica" />
-                    <Comment text={comment} userName="Chris Wei" />
+                    <Comment text={comment} userName="Maria Ladowska 1" />
+                    <Comment text={comment} userName="Steffany Jessica 2" />
+                    <Comment text={comment} userName="Chris Wei 3" />
+                    <Comment text={comment} userName="Maria Ladowska 4" />
+                    <Comment text={comment} userName="Steffany Jessica 5" />
+                    <Comment text={comment} userName="Chris Wei 6" />
+                    <Comment text={comment} userName="Maria Ladowska 7" />
+                    <Comment text={comment} userName="Steffany Jessica 8" />
+                    <Comment text={comment} userName="Chris Wei 9" />
                 </Slider>
-            </div>
+            </div> */}
+            {/* <div className={styles.carouselWrapper}>
+                <Carousel scrollMode="remainder" wrapAround={true} slidesToScroll={1} slidesToShow={3}>
+                    <Comment text={comment} userName="Maria Ladowska 1" />
+                    <Comment text={comment} userName="Steffany Jessica 2" />
+                    <Comment text={comment} userName="Chris Wei 3" />
+                    <Comment text={comment} userName="Maria Ladowska 4" />
+                    <Comment text={comment} userName="Steffany Jessica 5" />
+                    <Comment text={comment} userName="Chris Wei 6" />
+                    <Comment text={comment} userName="Maria Ladowska 7" />
+                    <Comment text={comment} userName="Steffany Jessica 8" />
+                    <Comment text={comment} userName="Chris Wei 9" />
+                </Carousel>
+            </div> */}
         </SectionWrapper>
 
     )
