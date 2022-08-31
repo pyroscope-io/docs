d94732a6847b27ecda3c6852593611e562487641
Dmitry Filimonov <dmitry@pyroscope.io>
2022-08-31 16:01:12 -0700

    better logos container

--- src/components/Homepage/HappyClients/index.jsx
+++ src/components/Homepage/HappyClients/index.jsx
@@ -2,24 +2,39 @@ import React from 'react';
 import styles from './styles.module.scss';
 import SectionWrapper from '../shared/SectionWrapper';
 
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+    return array;
+  }
+
+const ShuffleChildren = ({className, children}) => {
+    let childrenArr = React.Children.toArray(children);
+    shuffle(childrenArr);
+    return <div className={className}>{childrenArr}</div>;
+}
+
 const HappyClients = () => {
     return (
         <SectionWrapper>
             <div className={styles.section}>
                 <h2 className={styles.title}>These industry leaders profile with Pyroscope</h2>
-                <div className={styles.logoList}>
-                    <div className={styles.row}>
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/honeycomb.svg' />
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/line.svg' />
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/pingcap.svg' />
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/plaid.svg' />
-                    </div>
-                    <div className={styles.row}>
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/polkadot.svg' />
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/sensortower.svg' />
-                        <img className={styles.happyClientLogo} src='/img/homepage/logos/snapp.svg' />
-                    </div>
-                </div>
+                <ShuffleChildren className={styles.logoList}>
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/honeycomb.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/line.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/pingcap.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/plaid.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/polkadot.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/sensortower.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/snapp.svg' />
+                    <img className={styles.happyClientLogo} src='/img/homepage/logos/confluent.svg' />
+                </ShuffleChildren>
             </div>
         </SectionWrapper>
     )
