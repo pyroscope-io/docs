0399a379acdd4f60d29c422992c70863b02e328d
Dmitry Filimonov <dmitry@pyroscope.io>
2022-08-31 15:47:53 -0700

    adds logos

--- src/components/Homepage/HappyClients/index.jsx
+++ src/components/Homepage/HappyClients/index.jsx
@@ -9,12 +9,15 @@ const HappyClients = () => {
                 <h2 className={styles.title}>These industry leaders profile with Pyroscope</h2>
                 <div className={styles.logoList}>
                     <div className={styles.row}>
-                        <img src='/img/homepage/Google_logo.svg' />
-                        <img src='/img/homepage/Microsoft_logo.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/honeycomb.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/line.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/pingcap.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/plaid.svg' />
                     </div>
                     <div className={styles.row}>
-                        <img src='/img/homepage/Google_logo.svg' />
-                        <img src='/img/homepage/Microsoft_logo.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/polkadot.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/sensortower.svg' />
+                        <img className={styles.happyClientLogo} src='/img/homepage/logos/snapp.svg' />
                     </div>
                 </div>
             </div>
