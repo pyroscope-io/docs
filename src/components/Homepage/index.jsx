c057bd383e5b8a11343b7f9539d7f1fa2594950d
pavelpashkovsky <pavelpashkovsky@gmail.com>
2022-09-01 17:41:50 +0300

    chore: title

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -16,6 +16,7 @@ const HomePage = () => {
     return (
         <>
             <Helmet>
+                <title>Open Source Continuous Profiling Platform</title>
                 <meta name="description" content='Pyroscope - Open Source Continuous Profiling' />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <link rel="preload" href="/static/fonts/Sen-Regular.woff" as="font" type="font/woff2" crossorigin></link>
