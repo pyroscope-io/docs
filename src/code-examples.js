cba4894e5b6e007494d56f0f9de4eae63cf23580
Dmitry Filimonov <dmitry@pyroscope.io>
2022-09-13 19:49:50 -0700

    updates code examples

--- src/code-examples.js
+++ src/code-examples.js
@@ -1,15 +1,31 @@
-export const examplePprof = `cat cpuprofile.pb.gz | \\
-  curl "https://pyroscope.cloud/ingest?format=pprof&name=curl-test-app"
+const from = Math.floor(+new Date() / 1000) - 10;
+const until = from + 10;
+
+
+export const examplePprof = `# First get API_TOKEN from https://pyroscope.cloud/settings/api-keys
+
+# Ingest profiles in pprof format
+curl \\
+  -H "Authorization: Bearer $API_TOKEN" \\
+  --data-binary @cpuprofile.pb.gz \\
+  "https://pyroscope.cloud/ingest?format=pprof&from=${from}&until=${until}&name=my-app-name-pprof"
 `
 
-export const exampleJFR = `curl \\
-  -F jfr=@profile.jfr \\
-  "https://pyroscope.cloud/ingest?format=jfr&name=curl-test-app"
-  `
+export const exampleJFR = `# First get API_TOKEN from https://pyroscope.cloud/settings/api-keys
 
-export const exampleCollapsed = `printf "foo;bar 100\\n foo;baz 200" | \\
-  curl \\
+# Ingest profiles in JFR format
+curl \\
+  -H "Authorization: Bearer $API_TOKEN" \\
+  -F "jfr=@profile.jfr" \\
+  "https://pyroscope.cloud/ingest?format=jfr&from=${from}&until=${until}&name=my-app-name-jfr&units=samples&aggregationType=sum&sampleRate=100"
+`
+
+export const exampleCollapsed = `# First get API_TOKEN from https://pyroscope.cloud/settings/api-keys
+
+# Ingest profiles in collapsed format
+printf "foo;bar 100\\nfoo;baz 200" | curl \\
+  -H "Authorization: Bearer $API_TOKEN" \\
   --data-binary @- \\
-  'http://localhost:4040/ingest?name=curl-test-app'
+  'https://pyroscope.cloud/ingest?name=my-app-name-collapsed&from=${from}&until=${until}&'
 `
 
