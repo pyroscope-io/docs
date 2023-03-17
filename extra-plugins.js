b65884a225e75112c6965af3c916799c438cbfd9
Dmitry Filimonov <dmitry@pyroscope.io>
2023-03-17 11:43:23 -0700

    chore: removes intercom

--- extra-plugins.js
+++ extra-plugins.js
@@ -14,15 +14,6 @@ module.exports = [
           fbq('init', '223853962474196');
           fbq('track', 'PageView');
         </script>`,
-        `<script>
-            window.intercomSettings = {
-              app_id: "n4cr7229",
-            };
-        </script>`,
-        `<script>
-              // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/n4cr7229'
-              (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/n4cr7229';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-        </script><script src="https://hwydcnfx4l0w.statuspage.io/embed/script.js"></script>`,
       ]
     }
   ],
