0d7d273df612c44a850a44d6ff97d809d66955da
Dmitry Filimonov <dmitry@pyroscope.io>
2023-10-02 13:43:25 -0700

    chore: more redirects to grafana.com (#182)
    * more redirects
    
    * improvement

--- create-redirects.rb
+++ create-redirects.rb
@@ -1,14 +1,18 @@
 require 'json'
 
 JSON.parse(File.read("redirects.json")).each_pair do |from, to|
-  from = from.sub(/^\//, "").sub(/\/$/, "")
   next if to == ""
+
+  # normalize it first
+  from = from.sub(/^\//, "").sub(/\/$/, "")
+  # add slash unless this is root
+  key = [from, "index.html"].reject { |x| x == "" }.join("/")
 
   puts "Redirect 301 #{from} #{to}"
   system(
     "aws", "s3api", "put-object",
     "--bucket", "pyroscope.io",
-    "--key", "#{from}/index.html",
+    "--key", key,
     "--body", "static/slack/index.html",
     "--website-redirect-location", to)
 end
