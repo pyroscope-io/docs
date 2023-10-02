ccdbc7b96d90fef1cbd0e5bd1008d620907cfb94
Dmitry Filimonov <dmitry@pyroscope.io>
2023-10-02 13:34:15 -0700

    more redirects

--- create-redirects.rb
+++ create-redirects.rb
@@ -1,14 +1,18 @@
 require 'json'
 
 JSON.parse(File.read("redirects.json")).each_pair do |from, to|
-  from = from.sub(/^\//, "").sub(/\/$/, "")
   next if to == ""
+
+  # normalize it first
+  from = from.sub(/^\//, "").sub(/\/$/, "")
+  # add slash unless this is root
+  from = "#{from}/" unless from == ""
 
   puts "Redirect 301 #{from} #{to}"
   system(
     "aws", "s3api", "put-object",
     "--bucket", "pyroscope.io",
-    "--key", "#{from}/index.html",
+    "--key", "#{from}index.html",
     "--body", "static/slack/index.html",
     "--website-redirect-location", to)
 end
